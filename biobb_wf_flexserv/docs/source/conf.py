7ea61e626a773744a03cd314df1f41fa3ab4447c
gbayarri <genis.bayarri@irbbarcelona.org>
2023-08-01 10:43:24 +0200

    latex sphinx

--- biobb_wf_flexserv/docs/source/conf.py
+++ biobb_wf_flexserv/docs/source/conf.py
@@ -134,6 +134,7 @@ def setup(app):
     #app.add_stylesheet('theme_overrides.css')
     app.add_css_file('theme_overrides.css')
     app.add_js_file('theme_overrides.js')
+    app.add_js_file('https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML')
 
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
